df2d98081655c6a17efb0e282931a619ee30a7d9
Tuannguyenin2010 <mr.atuan2010@gmail.com>
2024-10-23 00:22:44 -0400

    install firestore

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,8 @@
 // src/firebase-config.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyA-ZAgk3QDJvz0MgVDmnEMGcdBqCrAsCjU",
@@ -13,3 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
