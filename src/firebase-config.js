39868b0aded47e41a031f2ccd527b33156fcb284
Tuannguyenin2010 <mr.atuan2010@gmail.com>
2024-10-22 23:15:52 -0400

    Add firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+// src/firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-ZAgk3QDJvz0MgVDmnEMGcdBqCrAsCjU",
+    authDomain: "react-app-6cd9e.firebaseapp.com",
+    projectId: "react-app-6cd9e",
+    storageBucket: "react-app-6cd9e.appspot.com",
+    messagingSenderId: "110951342357",
+    appId: "1:110951342357:web:535fcc51a66995b094891c"
+  };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
